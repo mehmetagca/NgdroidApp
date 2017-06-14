a08a0b568079cb3775ecca0427a8b17c76bee009
mehmetagca <mehmet.agca@yandex.com>
2017-06-14 16:40:15 +0300

    1. gun Kovboy asagi, yukari, saga, sola hareketi yapildi.

--- app/src/main/java/com/ngdroidapp/GameCanvas.java
+++ app/src/main/java/com/ngdroidapp/GameCanvas.java
@@ -1,9 +1,11 @@
 package com.ngdroidapp;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 import istanbul.gamelab.ngdroid.base.BaseCanvas;
-import istanbul.gamelab.ngdroid.util.Log;
+import istanbul.gamelab.ngdroid.util.Utils;
 
 
 /**
@@ -14,23 +16,104 @@ import istanbul.gamelab.ngdroid.util.Log;
 
 public class GameCanvas extends BaseCanvas {
 
+    private Bitmap tileset, spritesheet;
+    private Rect tilesrc, tiledst, spritesrc, spritedst;
+
+    private int kareno, animasyonno, animasyonyonu;
+
+    private int hiz, hizx, hizy, spritex, spritey;
+
+    int touchx, touchy;//Ekranda bastigimiz yerlerin koordinatlari
 
     public GameCanvas(NgApp ngApp) {
         super(ngApp);
     }
 
     public void setup() {
-        Log.i(TAG, "setup");
+        //Log.i(TAG, "setup");
+        tileset = Utils.loadImage(root,"images/tilea2.png");
+        tilesrc = new Rect();
+        tiledst = new Rect();
+
+        spritesheet = Utils.loadImage(root,"images/cowboy.png");
+
+        spritesrc = new Rect();
+        spritedst = new Rect();
+
+        kareno=0;
+
+        animasyonno = 1;
+
+        animasyonyonu = 0;
+
+        hiz = 16;
+        hizx = 0;
+        hizy = 0;
+        spritex = 0;
+        spritey = 0;
     }
 
 
 
     public void update() {
-        Log.i(TAG, "update");
+        //Log.i(TAG, "mehmet agca");
+
+
+       // tiledst.set(0,0,128,128);
     }
 
     public void draw(Canvas canvas) {
-        Log.i(TAG, "draw");
+        //Log.i(TAG, "draw");
+        tilesrc.set(0,0,64,64);
+
+        for (int i=0; i<getWidth(); i+=128)
+        {
+            for(int j=0; j<getHeight(); j+=128)
+            {
+                tiledst.set(i,j,i+128,j+128);
+                canvas.drawBitmap(tileset,tilesrc,tiledst,null);//yesil cimen zemini  tum ekrana cizme
+            }
+        }
+
+        spritex += hizx;
+        spritey += hizy;
+
+        if(spritex+256 > getWidth()) {//x ekseni icin sona geldimi kontrolu
+            spritex = getWidth() - 256;
+            animasyonno = 0;//sona gelince durma animasyonu
+        }
+        else if(spritex < 0)
+        {
+            spritex = 0;
+            animasyonno = 0;
+        }
+
+        if(spritey+256 > getHeight()){//y ekseni icin sona geldimi kontrolu
+            spritey = getHeight() -256;
+            animasyonno = 0;//sona gelince durma animasyonu
+        }
+        else if(spritey < 0)
+        {
+            spritey = 0;
+            animasyonno = 0;
+        }
+
+        if(animasyonno == 1)
+            kareno++;
+        else if(animasyonno == 0)
+            kareno = 0;
+
+        if(kareno > 8)
+            kareno=1;
+
+        if(hizx > 0)
+            animasyonyonu = 0;
+        else if(hizy > 0)
+            animasyonyonu = 9;
+
+        spritesrc.set(kareno*128, animasyonyonu*128,(kareno+1)*128, (animasyonyonu+1)*128);//Resimden aldigimiz koordinatlar
+        spritedst.set(spritex, spritey, spritex+256, spritey+256);//Ekrana cizilecegi koordinatlar
+        canvas.drawBitmap(spritesheet,spritesrc,spritedst,null);
     }
 
     public void keyPressed(int key) {
@@ -58,12 +141,53 @@ public class GameCanvas extends BaseCanvas {
     }
 
     public void touchDown(int x, int y) {
+        touchx = x;
+        touchy = y;
     }
 
     public void touchMove(int x, int y) {
     }
 
     public void touchUp(int x, int y) {
+        if((x - touchx) > 100)//asagi cektiysek
+        {
+            animasyonno = 1;
+            animasyonyonu = 0;
+
+            hizx = hiz;
+            hizy = 0;
+        }
+        else if((touchx - x) > 100)//yukari cektiysek
+        {
+            animasyonno = 1;
+            animasyonyonu = 1;
+
+            hizx = -hiz;
+            hizy = 0;
+        }
+        else if((y - touchy) > 100)//saga cektiysek
+        {
+            animasyonno = 1;
+            animasyonyonu = 9;
+
+            hizy = hiz;
+            hizx = 0;
+        }
+        else if((touchy - y) > 100)//sola cektiysek
+        {
+            animasyonno = 1;
+            animasyonyonu = 5;
+
+            hizy = -hiz;
+            hizx = 0;
+        }
+        else//mouse ile 100px den az bir degisim yaptiysak
+        {
+            animasyonno = 0;
+
+            hizx = 0;
+            hizy = 0;
+        }
     }
 
 
